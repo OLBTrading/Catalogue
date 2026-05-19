d17678d3172b93fc485971d662fb4e7ddff9bd37
Oscar Butler <oscarbutler>
2026-05-19 12:24:54 +0100

    Add product

--- src/data/Product.js
+++ src/data/Product.js
@@ -1,7 +1,7 @@
 import draytonImg from "../assets/images/draytonlp711.jpg"
 import keyboardImg from "../assets/images/bc-axb-131-bn-prsh.jpg"
 import showerImg from "../assets/images/ax-sir-mfsp-cp-1.webp"
-
+import salus210 from "../assets/images/SALUS EP210.jpg"
 const products = [
   {
     id: 1,
@@ -46,6 +46,19 @@ const products = [
     image: showerImg,
 
     ebayLink: "https://www.ebay.co.uk/",
+  },
+  {
+    id: 4,
+
+    slug: "Salus 210",
+
+    name: "Salus Programmer Timer Control Water Heating Replaces, White - EP210",
+
+    description: "MPN EP210 Year Manufactured Unknown Wire Configuration 7-Wire Battery Size 3V Button Cell Compatible Product",
+
+    image: salus210,
+    
+    ebayLink: "https://www.ebay.co.uk/itm/287285499764",
   },
 ]
 
