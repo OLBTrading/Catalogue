575d0cbfc187ee9dd767ce7de32cf3f9d99937c9
Oscar Butler <oscarbutler>
2026-05-19 18:10:48 +0100

    Add products

--- src/data/Product.js
+++ src/data/Product.js
@@ -2,64 +2,297 @@ import draytonImg from "../assets/images/draytonlp711.jpg"
 import keyboardImg from "../assets/images/bc-axb-131-bn-prsh.jpg"
 import showerImg from "../assets/images/ax-sir-mfsp-cp-1.webp"
 import salus210 from "../assets/images/SALUS EP210.jpg"
+import motorvalve from "../assets/images/7b9a75dd-3d63-4048-b2c3-7dfb95931a80.png"
+import eastbrook from "../assets/images/79.2012_0.png"
+import elo0009 from "../assets/images/6177e339b1823414551992.jpg"
+import arcovalve from "../assets/images/abe6614e17e701b39afd1a479b0ffa20998015c1.jpg"
+import axiro100cccp from "../assets/images/ax-iro-100-cc-cp-prsh.jpg"
+import axiro130k from "../assets/images/ax-iro-130_k-prsh.jpg"
+import axsirmfspcp from "../assets/images/ax-sir-mfsp-cp-11.jpg"
+import basin202117ins from "../assets/images/Basin_Trap_202117-INS_1.jpg"
+import brw75t from "../assets/images/BRW75T_w2.jpg"
+import basomonobloc from "../assets/images/Francis Pegler Haze Monobloc Basin Mixer Tap with Click Clack Waste.jpg"
+import fp4g4010 from "../assets/images/francis.png"
+import inst13010 from "../assets/images/inst13010.jpg"
+import lif130kcp from "../assets/images/lif-130_k-cp_prsh.jpg"
+import airbrickblue from "../assets/images/manthorpe-airbrick-blue-black-b034027-1__97643.jpg"
+import weepvent from "../assets/images/manthorpe-weep-vent-buff-g950bf-b034004-1__10162.jpg"
+import meriden3tap from "../assets/images/meriden-3-tap-hole-basin-mixer-tap-set-matt-black-1-260994-p.webp"
+import insto2028 from "../assets/images/Sanitaryware_Kinsley_INST02028_Image1.jpg"
+import lev181 from "../assets/images/lev-181-prsh.jpg"
+import lev185b from "../assets/images/lev-185b-prsh.jpg"
+
 const products = [
   {
     id: 1,
-
-    slug: "drayton-lp711",
-
-    name: "Drayton LP711",
-
-    description:
-      "Reliable heating programmer with simple scheduling controls.",
-
-    image: draytonImg,
-
-    ebayLink: "https://www.ebay.co.uk/",
+    slug: "towel-ring-lev-181-cp",
+    name: "Towel Ring, Finished In A High-Shine Polished Chrome LEV-181-C/P",
+    description: "",
+    image: "lev181",
+    ebayLink: "",
   },
 
   {
     id: 2,
-
-    slug: "mechanical-keyboard",
-
-    name: "Mechanical Keyboard",
-
-    description:
-      "Premium mechanical keyboard with responsive tactile switches.",
-
-    image: keyboardImg,
-
-    ebayLink: "https://www.ebay.co.uk/",
+    slug: "life-bath-shower-mixer",
+    name: "LIFE 2 Hole Bath Shower Mixer With Shower Kit LIF-130+K-C/P",
+    description: "",
+    image: "lif130kcp",
+    ebayLink: "",
   },
 
   {
     id: 3,
-
-    slug: "modern-shower",
-
-    name: "Modern Shower",
-
-    description:
-      "Luxury chrome shower system with modern minimalist design.",
-
-    image: showerImg,
-
-    ebayLink: "https://www.ebay.co.uk/",
+    slug: "francis-pegler-haze-basin-mixer",
+    name: "Francis Pegler Haze Single Lever Monobloc Basin Mixer 4G4003",
+    description: "",
+    image: "basemonobloc",
+    ebayLink: "",
   },
+
   {
     id: 4,
+    slug: "drayton-lp711",
+    name: "Drayton Lp711 7 Day Heating Timeswitch",
+    description: "",
+    image: "draytonImg",
+    ebayLink: "",
+  },
 
-    slug: "Salus 210",
+  {
+    id: 5,
+    slug: "3-port-motorised-zone-valve",
+    name: "3 Port Motorised Zone Valve 22mm ESZV223LM Central Heating Mid Position Valve",
+    description: "",
+    image: "motorvalve",
+    ebayLink: "",
+  },
 
+  {
+    id: 6,
+    slug: "esi-touch-thermostat",
+    name: "Esi Touch Twin Channel Programmable Room Thermostat ESRTP4TOUCH",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 7,
+    slug: "instinct-bottle-trap",
+    name: 'Instinct 1¼" Round Modern Brass Bottle Trap 202117-INS',
+    description: "",
+    image: "basin202117ins",
+    ebayLink: "",
+  },
+
+  {
+    id: 8,
+    slug: "francis-pegler-sink-mixer",
+    name: "Francis Pegler Haze Dual Control Monobloc Sink Mixer Tubular Spout 4G4010",
+    description: "",
+    image: "fp4g4010",
+    ebayLink: "",
+  },
+
+  {
+    id: 9,
+    slug: "vado-irlo-basin-mixer",
+    name: "VADO Irlo Mono Basin Mixer + Push Waste AX-IRO-100/CC-CP",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 10,
+    slug: "salus-ep210",
     name: "Salus Programmer Timer Control Water Heating Replaces, White - EP210",
+    description: "",
+    image: "salus210",
+    ebayLink: "",
+  },
 
-    description: "MPN EP210 Year Manufactured Unknown Wire Configuration 7-Wire Battery Size 3V Button Cell Compatible Product",
+  {
+    id: 11,
+    slug: "blanco-camia",
+    name: "Blanco Camia Chrome Twin Lever No. 525334",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
 
-    image: salus210,
-    
-    ebayLink: "https://www.ebay.co.uk/itm/287285499764",
+  {
+    id: 12,
+    slug: "scott-james-mixer-tap",
+    name: "Scott & James Twin Lever Mixer Tap – Brushed – ELO0009",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 13,
+    slug: "toilet-seat-soft-close",
+    name: "MFSSA0002 TOILET SEAT SOFT CLOSE SEAT WRAP",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 14,
+    slug: "cata-canopy-hood",
+    name: "Cata 52cm Canopy Hoods UBCAN52SV.1",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 15,
+    slug: "vado-irlo-bath-filler",
+    name: "Vado Irlo Deck Mounted Bath Filler Tap AX-IRO-137-CP",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 16,
+    slug: "vado-irlo-bath-shower-mixer",
+    name: "Vado Irlo Deck Mounted Bath/Shower Mixer + Shower Kit AX-IRO-130+K-CP",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 17,
+    slug: "kinsley-soft-close-seat",
+    name: "Kinsley Quick Release Soft Close Seat + Cover INST02028",
+    description: "",
+    image: "insto2028",
+    ebayLink: "",
+  },
+
+  {
+    id: 18,
+    slug: "manthorpe-weep-vent",
+    name: "MANTHORPE WEEP VENT BUFF G950BF B034004 x50",
+    description: "",
+    image: "weepvent",
+    ebayLink: "",
+  },
+
+  {
+    id: 19,
+    slug: "meriden-basin-mixer",
+    name: "Meriden 3 Tap Hole Basin Mixer Tap Set Matt Black",
+    description: "",
+    image: "meriden3tap",
+    ebayLink: "",
+  },
+
+  {
+    id: 20,
+    slug: "niagara-shower-set",
+    name: "Niagara Equate Round Thermostatic Shower Set – Brushed Brass 9301BRS",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 21,
+    slug: "cata-induction-hob",
+    name: "UBIND60MT CATA 100 - 60cm 4 Zone Induction Hob",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 22,
+    slug: "manthorpe-airbrick",
+    name: "MANTHORPE AIRBRICK BLUE-BLACK X20 G930BLB",
+    description: "",
+    image: "airbrickblue",
+    ebayLink: "",
+  },
+
+  {
+    id: 23,
+    slug: "vado-sirkel-shower-package",
+    name: "VADO Multi Function Thermostatic Shower Package - Sirkel - AX-SIR-MFSP-CP",
+    description: "",
+    image: "axsirmfspcp",
+    ebayLink: "",
+  },
+
+  {
+    id: 24,
+    slug: "timco-round-wire-nails",
+    name: "TIMCO Round Wire Nails - Bright 75 x 3.75mm (QTY: 4 PACKS)",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 25,
+    slug: "3-port-mechanised-zone-valve",
+    name: "3 Port Mechanised Zone Valve 22mm ESZV223LM",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 26,
+    slug: "arco-thermostatic-valve",
+    name: "Arco Single Outlet Two Handle Thermostatic Concealed Valve SKU: 6001",
+    description: "",
+    image: "arcovalve",
+    ebayLink: "",
+  },
+
+  {
+    id: 27,
+    slug: "eastbrook-haymarket-bath-filler",
+    name: "Eastbrook Haymarket Bath Filler Black No. 79.2012",
+    description: "",
+    image: "eastbrook",
+    ebayLink: "",
+  },
+
+  {
+    id: 28,
+    slug: "blitz-bath-filler",
+    name: "BLITZ Bath Filler Chrome BTZ BF C",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 29,
+    slug: "axbridge-bath-mixer",
+    name: "Axbridge Bath Mixer Tap with Shower Kit, Cross Handles BC-AXB-131-BN",
+    description: "",
+    image: "",
+    ebayLink: "",
+  },
+
+  {
+    id: 30,
+    slug: "level-towel-shelf",
+    name: 'LEVEL Bathroom Towel Shelf with Towel Rail 550mm (22") LEV-185B-C/P',
+    description: "",
+    image: "lev185b",
+    ebayLink: "",
   },
 ]
+
 
 export default products
